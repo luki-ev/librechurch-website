0a8d53003e7c0d7cf2c05aa84fac249c420f2ae9
Johannes Brakensiek <johannes@luki.org>
2018-12-19 21:11:10 +0100

    [change] Some texts [add] Preparation for metadata [fix] There scrolling script made very stupidly

--- src/theme/js/app.js
+++ src/theme/js/app.js
@@ -65,47 +65,49 @@ document.addEventListener('DOMContentLoaded',function(){
 	if ( 'querySelector' in document && 'addEventListener' in window && Array.prototype.forEach ) {
 		// Function to animate the scroll
 		var smoothScroll = function (anchor, duration) {
-		// Calculate how far and how fast to scroll
-		var startLocation = window.pageYOffset;
-		var endLocation = anchor.offsetTop - 40; // Remove 40 pixels for padding
-		var distance = endLocation - startLocation;
-		var increments = distance/(duration/16);
-		var stopAnimation;
-		// Scroll the page by an increment, and check if it's time to stop
-		var animateScroll = function () {
-			window.scrollBy(0, increments);
-			stopAnimation();
-		};
-	// If scrolling down
-		if ( increments >= 0 ) {
-		// Stop animation when you reach the anchor OR the bottom of the page
-			stopAnimation = function () {
-				var travelled = window.pageYOffset;
-				if ( (travelled >= (endLocation - increments)) || ((window.innerHeight + travelled) >= document.body.offsetHeight) ) {
-					clearInterval(runAnimation);
-				}
-			};
-		}
+            // Calculate how far and how fast to scroll
+            var startLocation = window.pageYOffset;
+            var endLocation = anchor.offsetTop - 40; // Remove 40 pixels for padding
+            var distance = endLocation - startLocation;
+            var increments = distance/(duration/16);
+            
+            // Scroll the page by an increment, and check if it's time to stop
+            var animateScroll = function () {
+                window.scrollBy(0, increments);
+                
+                var travelled = window.pageYOffset;
+                
+                // Scrolling down or up or end of document
+                if ( (increments >= 0 && (travelled >= (endLocation - increments))) ||
+                        ( increments < 0 && (travelled <= (endLocation - increments)) ) ||
+                        ((window.innerHeight + travelled) >= document.body.offsetHeight) ) {
+                    clearInterval(runAnimation);
+                }
+            };
+            
             // Loop the animation function
             var runAnimation = setInterval(animateScroll, 16);
         };
+        
 		// Define smooth scroll links
 		var scrollToggle = document.querySelectorAll('.scroll');
 		// For each smooth scroll link
 		[].forEach.call(scrollToggle, function (toggle) {
 			// When the smooth scroll link is clicked
 			toggle.addEventListener('click', function(e) {
-		// Prevent the default link behavior
-		e.preventDefault();
-		// Get anchor link and calculate distance from the top
-		var dataTarget = document.querySelector('.landing__section');
-		var dataSpeed = toggle.getAttribute('data-speed');
-	 	// If the anchor exists
-		if (dataTarget) {
-			// Scroll to the anchor
-				smoothScroll(dataTarget, dataSpeed || 700);
-			}
-		}, false);
+                // Prevent the default link behavior
+                e.preventDefault();
+                
+                var dataID = toggle.getAttribute('href');
+                var dataTarget = document.querySelector(dataID + ".landing__section");
+                var dataSpeed = toggle.getAttribute('data-speed');
+                
+                // If the anchor exists
+                if (dataTarget) {
+                // Scroll to the anchor
+                    smoothScroll(dataTarget, dataSpeed || 700);
+                }
+            }, false);
 		});
 	}
 
