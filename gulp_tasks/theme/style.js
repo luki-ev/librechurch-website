6a870c5dbcb9da9dd0530773d640925ccd2daafe
Christoph Settgast <csett86@web.de>
2021-05-12 17:17:40 +0200

    move autoprefixer config to default browserslist

--- gulp_tasks/theme/style.js
+++ gulp_tasks/theme/style.js
@@ -10,7 +10,7 @@ const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 
 gulp.task('theme_style', function() {
-	const plugins = [ autoprefixer({browsers: ['last 2 versions','ie 9']}), cssnano() ];
+	const plugins = [ autoprefixer(), cssnano() ];
 	return gulp.src('./src/theme/sass/style.scss')
 	.pipe(sass().on('error', sass.logError))
 	.pipe(postcss([flexibility]))
