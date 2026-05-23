46387a0a856b38b09020a97a7e42ef8f0fffa67e
Christoph Settgast <csett86_git@quicksands.de>
2026-05-23 10:31:19 +0200

    it was a static html page anyway, remove ejs templating

--- app.js
+++ app.js
@@ -3,17 +3,14 @@ const app = express();
 const path = require('path');
 const morgan = require('morgan');
 
-// Convert assets location based on what type of gulp task we are running
-app.locals.asset = function(assetLocation, assetTheme){ return assetLocation; }
-app.set('view engine', 'ejs'); // Set template engine to ejs
-
-// Theme pages
-app.set('views', [path.join(__dirname, 'src/theme/')]);
+// Theme assets
 app.use(express.static(path.join(__dirname, 'src/theme/public/')));
 app.use(express.static(path.join(__dirname, 'src/theme/assets')));
 app.use(morgan('tiny'));
 
-// Theme pages
-app.get('/', (req,res) => res.render('index')); // Landing page
+// Static index page
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'src/theme/index.html'));
+});
 
 app.listen(3000, () => console.log('Evie app listening on port 3000')); // Initialize the express server
