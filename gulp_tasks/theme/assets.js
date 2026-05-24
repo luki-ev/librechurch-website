48a0c8bb518d738018a0050b19f0ae8a93c2c68d
Christoph Settgast <csett86_git@quicksands.de>
2026-05-24 22:37:01 +0200

    remove src from pages

--- gulp_tasks/theme/assets.js
+++
@@ -1,9 +0,0 @@
-// theme_assets
-// Move the necessary images to the theme folder ('Evie/assets')
-
-const gulp = require('gulp');
-
-gulp.task('theme_assets', function(done){
-	return gulp.src(['src/theme/assets/**/*']).pipe(gulp.dest('public/')); // Transfer every asset
-	//return gulp.src(['./README.md',"./LICENSE"]).pipe(gulp.dest('Evie/')); // Transfer readme and license
-});
